cef6f6f0a9268236d7a347c0bbcf94f4939ceb68
Jacek-Kapral <jacek.kapral@proton.me>
2023-01-14 09:46:13 +0100

    Second version of program, including decrypting

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Excercise 1 from lab no. 3 of Identity and Access Management course on WSB
 
 First commit - a simple form of python script to encode string given by user. Uses built in base64 library.
+
+Next version - added a choice for encrypting, decrypting and exiting the program, also added some cosmetic changes.
---
+++ lab03-1a.py
@@ -0,0 +1,25 @@
+import base64
+while True:
+	wybor = input('Jezeli chcesz zaszyfrowac swoj lancuch znakow, wpisz z i nacisnij enter.\nJezeli chcesz deszyfrowac swoj lancuch znakow, wpisz d i nacisnij enter.\nJezeli chcesz wyjsc z programu, wpisz exit i nacisnij enter.')
+	
+	if wybor == 'z':
+		do_zakodowania = input("Podaj lancuch znakow do zaszyfrowania: ")
+		zakodowany = base64.b64encode(do_zakodowania.encode())
+		print("Twoj zaszyfrowany lancuch znakow wyglada tak: ", zakodowany.decode())
+	
+	elif wybor == 'd':
+		do_zdekodowania = input("Podaj lancuch znakow do deszyfrowania: ")
+		zdekodowany = base64.b64decode(do_zdekodowania.encode()).decode()
+		print("Twoj lancuch pod deszyfrowaniu wyglada tak: ", zdekodowany)
+		
+	elif wybor == 'exit':
+		break
+	
+	else:
+		print("Zly wybor, wpisz z dla zaszyfrowania lub d dla deszyfrowania.")
+
+		
+		
+#do_zakodowania = input("Podaj lancuch znakow do zaszyfrowania: ")
+#zakodowany = base64.b64encode(do_zakodowania.encode())
+#print("Twoj zaszyfrowany lancuch znakow wyglada tak: ", zakodowany.decode())
